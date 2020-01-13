9ec48650e62e2fea8ec2584cb4cb5876dded251d
bmv <leeloo1899@gmail.com>
2020-01-13 20:06:11 +0700

    update

--- src/DecimalToBinary/DecimalToBinary.java
+++ src/DecimalToBinary/DecimalToBinary.java
@@ -5,13 +5,9 @@ import java.util.Stack;
 public class DecimalToBinary {
 
     public void convertBinary(int num) {
-
         Stack<Integer> stack = new Stack<Integer>();
-
         while (num != 0) {
-            //thực hiện phép chia lấy phần dư cho 2.
             int d = num % 2;
-            // thêm vào stack.
             stack.push(d);
             num /= 2;
         }
@@ -22,7 +18,6 @@ public class DecimalToBinary {
     }
 
     public static void main(String[] args) {
-
         int decimalNumber = 35;
         System.out.print("Hệ nhị phân của " + decimalNumber + " là :");
         new DecimalToBinary().convertBinary(decimalNumber);
