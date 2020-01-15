a2216765991e0867db6468d8032466e383064301
bmv <leeloo1899@gmail.com>
2020-01-15 09:34:45 +0700

    update

--- src/Dictionary/CountWord.java
+++ src/Dictionary/CountWord.java
@@ -1,4 +1,35 @@
 package Dictionary;
 
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
 public class CountWord {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the paragraph: ");
+        String word = scanner.nextLine();
+        String[] strings = word.split("\\s");
+
+        HashMap<String, Integer> newMap = new HashMap<>();
+        putHashMap(strings,newMap);
+
+        Set<String> key = newMap.keySet();
+        for (String keys : key) {
+            System.out.println(keys+"    "+newMap.get(keys));
+        }
+
+    }
+    public static void putHashMap(String[] strings,HashMap<String,Integer>newMap){
+        for (int i = 0; i < strings.length; i++) {
+            int count = 0;
+            for (int j = 0; j < strings.length; j++) {
+                if (strings[i].equals(strings[j])) {
+                    count++;
+                }
+            }
+            newMap.put(strings[i], count);
+        }
+    }
 }
