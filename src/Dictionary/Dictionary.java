6397409ec0fc6a37b13b2bc8c5816bee57c653cd
bmv <leeloo1899@gmail.com>
2020-01-15 09:26:27 +0700

    update

--- src/Dictionary/Dictionary.java
+++ src/Dictionary/Dictionary.java
@@ -2,16 +2,35 @@ package Dictionary;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Dictionary {
     public static void main(String[] args) {
-        Map<String, String> dictionary = new HashMap<>();
-        dictionary.put("Hello","Xin Chao");
-        dictionary.put("Goodbye","Tam Biet");
-        dictionary.put("Home","Gia Dinh");
-        dictionary.put("Hello","Xin Chao cac ban");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter word: ");
+        String input = sc.nextLine();
+        Map<String, String> map = new HashMap<String, String>();
 
-        System.out.println(dictionary);
+        map.put("Hello", "Xin Chao");
+        map.put("Home", "Gia Dinh");
+        map.put("car", "oto");
+        map.put("phone", "dien thoai");
+        map.put("computer", "may tinh");
 
+        Dictionary diction = new Dictionary();
+        diction.find(input, (HashMap<String, String>) map);
+    }
+
+    public void find(String E, HashMap<String, String> map) {
+        for (String key : map.keySet()) {
+            if (map.containsKey(E)) {
+                System.out.println(map.get(E));
+                break;
+            } else {
+                System.out.println("404. Not found");
+                break;
+            }
+        }
     }
 }
+
